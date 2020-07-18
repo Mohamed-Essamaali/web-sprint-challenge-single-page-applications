cd8c67a772247ccd528da252e3e1acb93ede3650
Mohamed essamaali <mohamed.essamaali@gmail.com>
2020-07-17 17:43:42 -0700

    Added Header with Home button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import React from "react";
+import {Card, CardImg, Button, Input,Navbar} from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {Link,Route} from 'react-router-dom';
+import PizzaForm from './component/Form'
+import Home from './component/Home'
+
 
 const App = () => {
+
+
   return (
     <>
+    <Navbar color='info'>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <Link to = {'/'}>
+      <Button color= 'info'> Home</Button>
+      
+      </Link>
+    </Navbar>
+     
+      <Route exact path='/'/>
+
+      <Route path='/pizza'>
+        <PizzaForm/>
+        </Route>
+
+      <Card>
+        <CardImg src={require('./Assets/Pizza.jpg')} />
+        <Link to = {'/pizza'}>
+
+        <Button style={{position:'absolute',left:'50%',top:'50%',
+        padding:'1%',width:'15%',fontSize:'1.2rem', borderRadius:'6px'}}>Pizza</Button>
+      
+      </Link>
+      </Card>
+     
     </>
   );
 };
---
+++ src/component/Form.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PizzaForm = ()=>{
+
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default PizzaForm
